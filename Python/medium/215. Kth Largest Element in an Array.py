e73957d81a26c46d39fa6cb77e75862f3f1fe95a
Renascence <im.renascence@gmail.com>
2019-09-05 14:54:54 -0500

    2019-9-5

--- Python/medium/215. Kth Largest Element in an Array.py
+++ Python/medium/215. Kth Largest Element in an Array.py
@@ -17,3 +17,25 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort()
         return nums[-k]
+
+# use quick selcet avg O(N)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.quickSelect(nums, 0, len(nums) - 1, k - 1)
+    
+    def quickSelect(self, nums, l, r, k):
+        pivot = (l + r) // 2
+        pivotValue = nums[pivot]
+        nums[r], nums[pivot] = nums[pivot], nums[r]
+        storeIndex = l
+        for i in range(l, r):
+            if nums[i] > pivotValue:
+                nums[i], nums[storeIndex] = nums[storeIndex], nums[i]
+                storeIndex += 1
+        nums[r], nums[storeIndex] = nums[storeIndex], nums[r]
+        if storeIndex == k:
+            return nums[k]
+        if storeIndex > k:
+            return self.quickSelect(nums, l, storeIndex - 1, k)
+        else:
+            return self.quickSelect(nums, storeIndex + 1, r, k)
