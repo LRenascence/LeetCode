5796d8c2eb5bc4221d1c0a28c487b9ba11e798fa
Renascence <im.renascence@gmail.com>
2020-06-02 15:06:36 -0500

    2020-06-02

---
+++ events/June_challenge/week1/Invert Binary Tree.py
@@ -0,0 +1,25 @@
+"""
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+class Solution(object):
+    def invertTree(self, root):
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
